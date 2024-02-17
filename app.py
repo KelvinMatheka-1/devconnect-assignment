32f72063252e373a70d0196166c030e96c9b5284
Kelvin M Matheka <beingmatheka@gmail.com>
2024-02-17 15:22:57 +0300

    db

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
+import config
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{config.DATABASE_USER}:{config.DATABASE_PASSWORD}@localhost/test_database'
+db = SQLAlchemy(app)
+
 
 @app.route('/')
 def index():
---
+++ config.py
@@ -0,0 +1,3 @@
+DATABASE_USER = 'devconnect'
+DATABASE_PASSWORD = 'password'
+DATABASE_NAME = 'todo_database'
