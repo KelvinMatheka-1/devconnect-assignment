aa8f71a02046803c1e309c78d1c23509f6f681b0
Kelvin M Matheka <beingmatheka@gmail.com>
2024-02-17 16:27:04 +0300

    adding items

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import config
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -9,16 +9,30 @@ db = SQLAlchemy(app)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    item = db.Column(db.String, nullable=False)
+    content = db.Column(db.String, nullable=False)
     date_created  = db.Column(db.DateTime, default=datetime.utcnow)
     # completed = db.Column(db.Integer, default=0)
 
     def __repr__(self):
         return f"<Task {self.id}>"
     
-@app.route('/')
+@app.route('/', methods= ['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "there was an issue adding task"
+        
+    else:
+      tasks = Todo.query.order_by(Todo.date_created).all()
+      print(tasks)
+      return render_template('index.html', tasks=tasks)
 
 if __name__ == '__main__':
     app.run(debug=True)
